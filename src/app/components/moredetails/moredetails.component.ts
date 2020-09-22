de779a55bdb4890121db46ce4b7dda3b6693230a
Santiago Balbiani <santiagobalbiani@hotmail.com>
2020-09-22 13:29:22 -0300

    upd req 10 done

--- src/app/components/moredetails/moredetails.component.ts
+++ src/app/components/moredetails/moredetails.component.ts
@@ -2,14 +2,30 @@ import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { NavigationEnd, Router, ActivatedRoute } from "@angular/router";
+
+
+
+/* import { getViewById } from "tns-core-modules/ui/core/view";
+import { Label } from "tns-core-modules/ui/label";
+import { View } from "tns-core-modules/ui/core/view"; */
+
+import { Button } from "tns-core-modules/ui/button";
+import { EventData } from "tns-core-modules/data/observable";
+
+
 @Component({
     selector: "MoreDetails",
-    templateUrl: "./moredetails.component.html"
+    templateUrl: "./moredetails.component.html",
+    styleUrls: ["./moredetails.css"]
 })
 export class MoreDetailsComponent implements OnInit{
     
+   public message;
+
     constructor(private router: Router) {
         // Use the component constructor to inject providers.
+        this.message = "test1";
+        
     }
 
     ngOnInit(): void {
@@ -20,4 +36,16 @@ export class MoreDetailsComponent implements OnInit{
         const sideDrawer = <RadSideDrawer>app.getRootView();
         sideDrawer.showDrawer();
     }
-} 
+
+
+    onTap(args: EventData) {     
+         let button = args.object as Button;
+        /* let parent = button.parent; */     
+                this.message = "Bienvenido a la sección de detalles.";               
+                 if (button.android) {
+                    this.message += " Estas usando Android";
+                } 
+            }
+    }
+      /*     if (parent) {
+            let lbl = <Label>getViewById(parent, "cargarsaludo"); */
