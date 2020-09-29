c99e3df4161ebd00ef725d09b65d438281ec26cc
Santiago Balbiani <santiagobalbiani@hotmail.com>
2020-09-29 16:56:42 -0300

    termino tarea

--- src/app/components/productdetail/productdetail.component.ts
+++ src/app/components/productdetail/productdetail.component.ts
@@ -3,15 +3,17 @@ import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { NavigationEnd, Router, ActivatedRoute } from "@angular/router";
 import { ProductosService } from "~/app/domain/productos.service";
-import { PageRoute } from "nativescript-angular/router";
+import { registerElement } from "nativescript-angular/element-registry";
+registerElement("PullToRefresh", () => require("@nstudio/nativescript-pulltorefresh").PullToRefresh); 
+/* import { PageRoute } from "nativescript-angular/router"; */
 /* import { registerElement } from "nativescript-angular/element-registry";
 registerElement("PullToRefresh", () => require("@nstudio/nativescript-pulltorefresh").PullToRefresh); */
 /* import {PullToRefresh} from '@nstudio/nativescript-pulltorefresh'; */
 /* import { RadListView, ListViewEventData, PullToRefreshStyle } from "nativescript-ui-listview"; */
-import { Color } from "tns-core-modules/color";
+/* import { Color } from "tns-core-modules/color";
 import { EventData } from "tns-core-modules/data/observable";
 
-import { PullToRefresh } from '@nstudio/nativescript-pulltorefresh';
+import { PullToRefresh } from '@nstudio/nativescript-pulltorefresh'; */
 
 @Component({
     selector: "ProductDetail",
@@ -21,13 +23,13 @@ export class ProductDetail implements OnInit {
 
     private id:String = '';
     private aProduct;
-    private pruebaTest = [{id: "1", opinion: "Muy buen producto!", usuario:"Juan Perez", puntaje:"60"},{id: "2", opinion: "Muy mal producto!", usuario:"Juana Perez", puntaje:"45"},{id: "1", opinion: "buen producto!", usuario:"Juan Perez", puntaje:"50"}]
+    private opiniones = [];
     
     constructor(private router: Router, private route: ActivatedRoute, private productos: ProductosService) {
         // Use the component constructor to inject providers.
         this.route.params.subscribe(param => this.id = param.id);
         this.aProduct = productos.buscar().filter( prod => prod.id === this.id.toString())[0];
-        
+        this.opiniones = [{id: "1", opinion: "Muy buen producto!", usuario:"Juan Perez", puntaje:"60"},{id: "2", opinion: "Muy mal producto!", usuario:"Juana Perez", puntaje:"45"},{id: "1", opinion: "buen producto!", usuario:"Juan Perez", puntaje:"50"}];
     }
 
     ngOnInit(): void {
@@ -39,15 +41,19 @@ export class ProductDetail implements OnInit {
         sideDrawer.showDrawer();
     }
 
-     refreshList(args) {
+     onPull(args) {
         const pullRefresh = args.object;
+        this.setOpinion({id: "4", opinion: "A este producto le doy un" + Math.random(), usuario:"Luan Perrez", puntaje:"60"})
         setTimeout(function () {
+            
            pullRefresh.refreshing = false;
         }, 1000); 
    }
-
+    setOpinion(opinion){
+        this.opiniones.push(opinion);
+    }
     //Not used
     getDetail(){
-        return this.pruebaTest;
+        return this.opiniones;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
+import { registerElement } from "nativescript-angular/element-registry";
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 /* import { registerElement } from "nativescript-angular/element-registry";
 registerElement("PullToRefresh", () => require("@nstudio/nativescript-pulltorefresh").PullToRefresh); */
 import { AppModule } from "./app/app.module";
-
+/* import { PullToRefresh } from "@nstudio/nativescript-pulltorefresh"; */
+registerElement("PullToRefresh", () => require("@nstudio/nativescript-pulltorefresh").PullToRefresh); 
 platformNativeScriptDynamic().bootstrapModule(AppModule);
